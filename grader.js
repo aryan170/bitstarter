b55654fe08a6289e300ab77462ffbfa68617d62e
Nitin kalra <nitink_usc@yahoo.com>
2013-07-29 17:26:10 +0000

    Changed grade.js and checks.json

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://www.google.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -65,10 +67,42 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_url>', 'Url for the website', URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	var i = 0;
+	var flag_url = 0;
+	for (i=0;i<process.argv.length;i++) {
+		if (process.argv[i] == "--url") {
+			console.log("Url is used");
+		        flag_url = 1;
+		}
+	}
+        if (flag_url) {
+		// rest.get will return object
+		//showProps(url_html1, "url_html1");
+		var url_html = rest.get(program.url).on('complete',function(result) {
+			if (result instanceof Error) {
+				console.log('Error ' + result.message);
+			} else {
+				fs.writeFileSync("out.html",result);
+			}
+		});
+    		var checkJson = checkHtmlFile("out.html", program.checks);
+	} else {
+    		var checkJson = checkHtmlFile(program.file, program.checks);
+	}
+    	var outJson = JSON.stringify(checkJson, null, 4);
+    	console.log(outJson);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
+}
+
+function showProps(obj, objName) {
+  var result = "";
+  for (var i in obj) {
+    if (obj.hasOwnProperty(i)) {
+        result += objName + "." + i + " = " + obj[i] + "\n";
+	console.log("Result: " + result);
+    }
+  }
 }
